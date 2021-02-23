dcd0f946083a2f3937e060a93abdb27221a9008a
Maël Naccache Tüfekçi <contact@maeln.com>
2021-02-23 09:25:16 +0100

    lut for fibonacci.

--- src/dynamic_prog.rs
+++ src/dynamic_prog.rs
@@ -121,6 +121,17 @@ pub fn climb_stairs_dyn(n: i32) -> i32 {
     return steps[n as usize];
 }
 
+// LUT based one, just for fun.
+const LUT: &[i32] = &[
+    0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946,
+    17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578,
+    5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296,
+    433494437, 701408733, 1134903170, 1836311903,
+];
+pub fn climb_stairs_lut(n: i32) -> i32 {
+    return LUT[n as usize];
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -161,5 +172,14 @@ mod tests {
         assert_eq!(2, climb_stairs_dyn(2));
         assert_eq!(3, climb_stairs_dyn(3));
         assert_eq!(1836311903, climb_stairs_dyn(45));
+    }
+
+    #[test]
+    fn climbing_stairs_lut() {
+        assert_eq!(0, climb_stairs_lut(0));
+        assert_eq!(1, climb_stairs_lut(1));
+        assert_eq!(2, climb_stairs_lut(2));
+        assert_eq!(3, climb_stairs_lut(3));
+        assert_eq!(1836311903, climb_stairs_lut(45));
     }
 }
